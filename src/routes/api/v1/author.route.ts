cab6cacdc6b8d0e88c6f10fc586a99055e767ee6
highb33kay <hiibeekayvibe@gmail.com>
2024-03-04 04:05:50 +0100

    Add Swagger documentation for API endpoints

--- src/routes/api/v1/author.route.ts
+++ src/routes/api/v1/author.route.ts
@@ -18,21 +18,152 @@ import {
   loginSchema,
 } from "../../../middlewares/validations/author.zod";
 
+/**
+ * @swagger
+ * tags:
+ *   name: Authors
+ *   description: API endpoints for managing authors
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Author:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the author
+ *         fullname:
+ *           type: string
+ *           description: The full name of the author
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: The password of the author
+ *         bio:
+ *           type: string
+ *           description: The biography of the author (optional)
+ */
+
+/**
+ * @swagger
+ * /authors/:
+ *   get:
+ *     summary: Retrieve all authors
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: A list of authors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Author'
+ *   post:
+ *     summary: Create a new author
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Author'
+ *     responses:
+ *       '201':
+ *         description: Author created successfully
+ *       '400':
+ *         description: Bad request, invalid data supplied
+ *
+ * /authors/{authorId}:
+ *   get:
+ *     summary: Get an author by ID
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: authorId
+ *         required: true
+ *         description: ID of the author to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Author found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Author'
+ *       '404':
+ *         description: Author not found
+ *   put:
+ *     summary: Update an author by ID
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: authorId
+ *         required: true
+ *         description: ID of the author to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Author'
+ *     responses:
+ *       '200':
+ *         description: Author updated successfully
+ *       '404':
+ *         description: Author not found
+ *   delete:
+ *     summary: Delete an author by ID
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: authorId
+ *         required: true
+ *         description: ID of the author to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '204':
+ *         description: Author deleted successfully
+ *       '404':
+ *         description: Author not found
+ */
+
 import { validationMiddleware } from "../../../middlewares/validator";
 
 router.get("/authors/", authMiddleware, getAuthors);
+
 router.get("/author/:authorId", authMiddleware, getAuthor);
 router.post(
   "/register",
   validationMiddleware(authorCreateSchema),
   createAuthor
 );
+
 router.put(
   "/author/:authorId",
   authMiddleware,
   validationMiddleware(authorUpdateSchema),
   updateAuthor
 );
+
 router.delete("/author/:authorId", authMiddleware, deleteAuthor);
 
 router.post("/login", validationMiddleware(loginSchema), login);
--- src/routes/api/v1/book.route.ts
+++ src/routes/api/v1/book.route.ts
@@ -19,20 +19,174 @@ import {
 
 import { validationMiddleware } from "../../../middlewares/validator";
 
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: API endpoints for managing books
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The title of the book
+ *         author:
+ *           $ref: '#/components/schemas/Author'
+ *         publication_year:
+ *           type: string
+ *           format: date
+ *           description: The publication year of the book
+ *         isbn:
+ *           type: string
+ *           description: The ISBN of the book
+ *         authorId:
+ *           type: integer
+ *           format: int64
+ *           description: The ID of the author associated with the book
+ *         categoryId:
+ *           type: integer
+ *           format: int64
+ *           description: The ID of the category associated with the book
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Retrieve all books
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
 router.get("/books", authMiddleware, getBooks);
+
+/**
+ * @swagger
+ * /book/{bookId}:
+ *   get:
+ *     summary: Get a book by ID
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         description: ID of the book to retrieve
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Book found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       '404':
+ *         description: Book not found
+ */
 router.get("/book/:bookId", authMiddleware, getBook);
+
+/**
+ * @swagger
+ * /book/:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       '201':
+ *         description: Book created successfully
+ *       '400':
+ *         description: Bad request, invalid data supplied
+ */
+
 router.post(
   "/book/",
   authMiddleware,
   validationMiddleware(bookCreateSchema),
   createBook
 );
+
+/**
+ * @swagger
+ * /book/{bookId}:
+ *   put:
+ *     summary: Update a book by ID
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         description: ID of the book to update
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       '200':
+ *         description: Book updated successfully
+ *       '404':
+ *         description: Book not found
+ */
+
 router.put(
   "/book/:bookId",
   authMiddleware,
   validationMiddleware(bookUpdateSchema),
   updateBook
 );
+
+/**
+ * @swagger
+ * /book/{bookId}:
+ *   delete:
+ *     summary: Delete a book by ID
+ *     tags: [Books]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: bookId
+ *         required: true
+ *         description: ID of the book to delete
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '204':
+ *         description: Book deleted successfully
+ *       '404':
+ *         description: Book not found
+ */
+
 router.delete("/book/:bookId", authMiddleware, deleteBook);
 
 module.exports = router;
